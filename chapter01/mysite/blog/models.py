a2637216302463d9427ac88e9235e33236820483
lingchen1316@163.com <lingchen1316@163.com>
2022-03-26 23:39:47 +0800

    app blog list view fixed.

--- chapter01/mysite/blog/models.py
+++ chapter01/mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -51,6 +52,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        """
+        获取详细post的url路径.
+        :return:
+        """
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
 
 
--- chapter01/mysite/blog/views.py
+++ chapter01/mysite/blog/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 
 # Create your views here.
+
+
+def post_list(request):
+    """
+    获取post列表, 过滤状态为published.
+    :param request:
+    :return:
+    """
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    """
+    获取post的详细信息.
+    :param request:
+    :param year:
+    :param month:
+    :param day:
+    :param post:
+    :return:
+    """
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post', post})
+
