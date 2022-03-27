30a51f3596cb9302d7f622375a9eb2d8ab8da972
lingchen1316@163.com <lingchen1316@163.com>
2022-03-27 12:29:01 +0800

    posts pagination view fixed.

--- chapter01/mysite/blog/views.py
+++ chapter01/mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
@@ -11,8 +12,23 @@ def post_list(request):
     :param request:
     :return:
     """
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    objects_list = Post.published.all()
+    # 分页. 每页3个post.
+    paginator = Paginator(objects_list, 3)
+    # 获取当前页面数.
+    page = request.GET.get('page')
+    print('page: ' + str(page))
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果页面不是整数，则交付第一页.
+        print('No pages!')
+        posts = paginator.page(1)
+    except EmptyPage:
+        # 如果页面超出范围，请提交最后一页结果.
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
