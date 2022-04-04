e64a8221ab715c4472c268530040d29c067abcd6
lingchen1316@163.com <lingchen1316@163.com>
2022-04-04 20:21:08 +0800

    chapter4 account message display related fixed.

--- chapter04/bookmarks/account/views.py
+++ chapter04/bookmarks/account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
@@ -96,6 +97,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            # 信息设置.
+            messages.success(request, 'Profile updated successfully.')
+        else:
+            messages.error(request, 'Error updating your profile.')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
